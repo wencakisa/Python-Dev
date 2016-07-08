d26e2a3fa47cbfdff68e922229c548024432312e
wencakisa <wencakisa@gmail.com>
2016-07-08 16:14:07 +0300

    Added scripts for currency conversion and weather analisys

---
+++ url_requests/currency_convertor.py
@@ -0,0 +1,37 @@
+import sys
+import requests
+from decimal import Decimal
+
+API_URL = 'http://api.fixer.io/{}?base={}'
+API_TIMEOUT = 20
+
+
+def main():
+    base_currency = str(input('From: '))
+    base_amount = Decimal(input('Enter amount: '))
+    target_currency = str(input('To: '))
+    date = str(input('From date (yyyy/mm/dd): '))
+
+    print('. . . Getting exchange rates . . .')
+    resp = requests.get(API_URL.format(date, base_currency), timeout=API_TIMEOUT)
+
+    if resp.status_code != 200:
+        print('Unknown currency: {}'.format(base_currency))
+        return 1
+
+    exchange_rates = resp.json().get('rates', {})
+    exchange_rate_to_target_currency = exchange_rates.get(target_currency, None)
+
+    if exchange_rate_to_target_currency is not None:
+        exchange_rate_to_target_currency = Decimal(exchange_rate_to_target_currency)
+    else:
+        print('Unknown currency: {}'.format(target_currency))
+        return 1
+
+    target_amount = base_amount * Decimal(exchange_rate_to_target_currency)
+
+    print('{:.2f} {} = {:.2f} {}'.format(base_amount, base_currency, target_amount, target_currency))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ url_requests/forecast.py
@@ -0,0 +1,48 @@
+import sys
+import requests
+from datetime import datetime
+from decimal import Decimal
+
+API_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=965acdac1ae64cf06761bb563ad34d96'
+KELVIN_TO_CELSIUS_SUBTRACT = 273.15
+
+
+def main():
+    city = str(input('Enter a city: '))
+
+    print('. . . Getting weather information . . .')
+
+    resp = requests.get(API_URL.format(city))
+    city_info_dict = resp.json()
+
+    if city_info_dict.get('cod') == '404':
+        print('Unknown city: {}'.format(city))
+        return 1
+
+    main_info = city_info_dict.get('main')
+
+    temp = Decimal(main_info.get('temp') - KELVIN_TO_CELSIUS_SUBTRACT)
+    pressure = main_info.get('pressure')
+    humidity = main_info.get('humidity')
+
+    wind_info = city_info_dict.get('wind')
+
+    wind_speed = wind_info.get('speed')
+
+    print('''
+Info from {}
+Temperature: {:.2f}°C
+Pressure: {}p
+Humidity: {}%
+Wind speed: {}m/s
+'''.format(
+        datetime.now().replace(minute=0).strftime('%d-%m-%Y %H:%M'),
+        temp,
+        pressure,
+        humidity,
+        wind_speed
+    ))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
