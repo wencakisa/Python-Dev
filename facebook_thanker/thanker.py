6d00f2778ab2ba1aae7a52a7081d2a3498fdd36f
wencakisa <wencakisa@gmail.com>
2016-07-13 13:50:15 +0300

    Fixed

--- facebook_thanker/thanker.py
+++ facebook_thanker/thanker.py
@@ -2,10 +2,14 @@ import sys
 import requests
 
 with open('access_token.txt', 'r') as f:
-    ACCESS_TOKEN = str(f.readline())
+    lines = f.readlines()
+
+ACCESS_TOKEN = str(lines[0])
+FB_NAME = str(lines[1])
 
 GET_POSTS_URL = 'https://graph.facebook.com/me/feed?access_token=' + ACCESS_TOKEN
 POST_COMMENT_URL = 'https://graph.facebook.com/{}/comments?message={}&access_token=' + ACCESS_TOKEN
+LIKE_POST_URL = 'https://graph.facebook.com/{}/likes?access_token=' + ACCESS_TOKEN
 
 
 def main():
@@ -15,9 +19,9 @@ def main():
     posts_json = resp.json().get('data')
 
     post_requests = [
-        post.get('id')
+        post.get('id')  # Add the post ID
         for post in posts_json
-        if post.get('to') is not None  # If the post is from a friend
+        if post.get('from').get('name') != FB_NAME  # If the post is from a friend
         if post.get('comments') is None  # If there are no comments on this post
     ]
 
@@ -30,9 +34,10 @@ def main():
     print('. . . Posting comments . . .')
 
     for post_req_id in post_requests:
-        requests.post(POST_COMMENT_URL.format(post_req_id, comment))
+        requests.post(LIKE_POST_URL.format(post_req_id))  # Like the post
+        requests.post(POST_COMMENT_URL.format(post_req_id, comment))  # Comment the post
 
-    print('Done! Commented on {} posts.'.format(len(post_requests)))
+    print('Done! Liked and commented {} posts.'.format(len(post_requests)))
 
 if __name__ == '__main__':
     sys.exit(main())
