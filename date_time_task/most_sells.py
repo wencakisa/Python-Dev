8ad4f9f5f271323397ed867d4701609fc3af5aa1
wencakisa <wencakisa@gmail.com>
2016-07-16 14:43:53 +0300

    Simplified and fixed bugs

--- date_time_task/most_sells.py
+++ date_time_task/most_sells.py
@@ -1,91 +1,99 @@
-from datetime import datetime
 import logging
+from collections import defaultdict
+import csv
+import iso8601
+import sys
 
 logging.basicConfig(filename='errors.log', level=logging.DEBUG)
 
 
-def split_csv_file(file):
-    splitted = []
+def main():
+    try:
+        input_filename = './sales.csv'
+        sales_list = split_csv_file(input_filename)
 
-    with open(file) as f:
-        lines = f.readlines()
+        most_date_sales = get_most_date_sales(sales_list)
+        most_hour_sales = get_most_hour_sales(sales_list)
 
-        for ind, line in enumerate(lines):
-            if line.rstrip():
-                splitted.append(line.split(','))
+        print(most_sales_analysis(most_date_sales, most_hour_sales))
+
+        return 0
+    except Exception as e:
+        print('Error occurred: {}'.format(str(e)))
+        return 1
+
+
+def split_csv_file(input_filename: str) -> list:
+    result = []
+
+    with open(input_filename, encoding='utf-8') as f:
+        for line_index, line in enumerate(csv.reader(f)):
+            if line:
+                result.append(line)
             else:
-                logging.warning('Invalid line detected on {}'.format(ind + 1))
+                logging.warning('Empty line: {}'.format(line_index))
 
-    return splitted
+    return result
 
 
-def get_most_date_sales(sales):
-    date_sales = {}
+def load_sales_data_by_date(sales_list: list) -> defaultdict:
+    date_sales = defaultdict(list)
 
-    for info in sales:
-        date_time = datetime.strptime(info[0], '%Y-%m-%d %H:%M:%S')
-        formatted_date = date_time.date().strftime('%Y-%m-%d')
+    for info in sales_list:
+        d = iso8601.parse_date(info[0]).date()
         price = float(info[-1].strip())
 
-        if formatted_date in date_sales:
-            date_sales[formatted_date].append(price)
-        else:
-            date_sales[formatted_date] = [price]
+        date_sales[d].append(price)
 
-    date_sales_sum = {date: sum(sales) for date, sales in date_sales.items()}
-    max_date_sales_sum = max(date_sales_sum.values())
+    return {date: sum(sales) for date, sales in date_sales.items()}
 
-    for date, sales_sum in date_sales_sum.items():
+
+def load_sales_data_by_hour(sales_list: list) -> defaultdict:
+    hour_sales = defaultdict(list)
+    most_sales_date = get_most_date_sales(sales_list)[0]
+
+    for info in sales_list:
+        d = iso8601.parse_date(info[0])
+        price = float(info[-1].strip())
+
+        if str(d.date()) == str(most_sales_date):
+            hour_sales[d.hour].append(price)
+
+    return {hour: sum(sales) for hour, sales in hour_sales.items()}
+
+
+def get_most_date_sales(sales_list: list) -> tuple:
+    date_sales = load_sales_data_by_date(sales_list)
+    max_date_sales_sum = max(date_sales.values())
+
+    for date, sales_sum in date_sales.items():
         if sales_sum == max_date_sales_sum:
-            return date, round(sales_sum, 2)
+            return date, sales_sum
 
 
-def get_most_hour_sales(sales):
-    hour_sales = {}
-    most_sales_date = get_most_date_sales(sales)[0]
+def get_most_hour_sales(sales_list: list) -> tuple:
+    hour_sales = load_sales_data_by_hour(sales_list)
+    max_hour_sales_sum = max(hour_sales.values())
 
-    for info in sales:
-        date_time = datetime.strptime(info[0], '%Y-%m-%d %H:%M:%S')
-        formatted_date = date_time.date().strftime('%Y-%m-%d')
-        price = float(info[-1].strip())
-
-        if formatted_date == most_sales_date:
-            if date_time.hour in hour_sales:
-                hour_sales[date_time.hour].append(price)
-            else:
-                hour_sales[date_time.hour] = [price]
-
-    hour_sales_sum = {hour: sum(sales) for hour, sales in hour_sales.items()}
-    max_hour_sales_sum = max(hour_sales_sum.values())
-
-    for hour, sales_sum in hour_sales_sum.items():
+    for hour, sales_sum in hour_sales.items():
         if sales_sum == max_hour_sales_sum:
-            return hour, round(sales_sum, 2)
+            return hour, sales_sum
 
 
-def get_most_sales(sales):
-    most_sales_date = get_most_date_sales(sales)[0]
-    most_sales_date_amount = get_most_date_sales(sales)[1]
-    most_sales_hour = get_most_hour_sales(sales)[0]
-    most_sales_hour_amount = get_most_hour_sales(sales)[1]
+def most_sales_analysis(most_date_sales: tuple, most_hour_sales: tuple) -> str:
+    most_sales_date, most_sales_date_amount = most_date_sales
+    day_of_week = most_sales_date.strftime('%A')
 
-    date_time = datetime.strptime(most_sales_date, '%Y-%m-%d')
-    day_of_week = date_time.date().strftime('%A')
+    most_sales_hour, most_sales_hour_amount = most_hour_sales
 
-    most_sales_date_output = 'Most sales on {} ({}): {}$'.format(
+    most_sales_date_output = 'Most sales on {} ({}): {:.2f}$'.format(
         most_sales_date, day_of_week, most_sales_date_amount
     )
-    most_sales_hour_output = 'Most sales between {}-{}h: {}$'.format(
+    most_sales_hour_output = 'Most sales between {}-{}h: {:.2f}$'.format(
         most_sales_hour, most_sales_hour + 1, most_sales_hour_amount
     )
 
     return '{}\n{}'.format(most_sales_date_output, most_sales_hour_output)
 
-
-def main():
-    file = './sales.csv'
-    sales = split_csv_file(file)
-    print(get_most_sales(sales))
-
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
